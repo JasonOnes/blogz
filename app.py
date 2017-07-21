1dfec9e5648cce4ae652fd040cc789225806b009
JasonOnes <jasonr.jones14@gmail.com>
2017-07-21 08:39:19 -0700

    sql psw

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+phpmysql://blogz:osprey11@localhost:3306/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+phpmysql://blogz:summer69@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG'] = True
 
